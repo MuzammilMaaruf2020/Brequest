442fd460e614c5a1541633a39c30a91fe6107dac
MuzammilMaaruf2020 <80328947+MuzammilMaaruf2020@users.noreply.github.com>
2023-06-19 17:42:21 +0000

    added dynamic data

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template,jsonify
 app = Flask(__name__,template_folder='template')
+
+
+Works = [{
+  'id' : 1,
+  'category': 'Carpentry works'},
+
+  
+  {'id' : 2,
+  'category': 'Plumbing works'},
+
+  
+  {'id' : 3,
+  'category': 'Masonry works'
+  
+}
+
+  
+]
+
+@app.route("/api/works")
+def list_works():
+  return jsonify(Works)
 
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html',works=Works)
 
 
 if __name__ == '__main__':
